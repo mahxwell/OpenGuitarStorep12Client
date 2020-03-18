573d9fce36234812203335c0a60f79ad1fe6d078
Mahxwell <maxime.soro@gmail.com>
2020-03-18 15:53:44 +0100

    Add header component

--- ClientUIOpenGuitarStore/src/app/app.module.ts
+++ ClientUIOpenGuitarStore/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {UserService} from './services/user.service';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
